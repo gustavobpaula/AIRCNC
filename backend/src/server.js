badcac9e748a6182e445d3fbbdbcdf7c2520069c
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2019-11-04 22:06:26 -0300

    feat: :rocket: User backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 
 const app = express();
+
+mongoose.connect('mongodb+srv://rocketseat:rocketseat@rocketseat-nur5l.mongodb.net/airbnc?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
 
 app.use(express.json());
 app.use(routes);
